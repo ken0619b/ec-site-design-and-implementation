7ab3177d83e991e843df3c3123f4495b93b76032
Kenichi Ukai <ken151102gmail.com>
2021-01-06 14:29:55 +0900

    delete unused file

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Story from "./components/story";
 import ProductList from "./components/product-list";
 import { createGlobalStyle } from "styled-components";
 
-import products from "./product_data";
-
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 body {
